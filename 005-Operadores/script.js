2c90acb5532d456640b2d6fc08f84379b25a68bc
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-08-20 21:15:10 -0300

    Add JavaScript operators examples in script.js

---
+++ 005-Operadores/script.js
@@ -0,0 +1,39 @@
+// ==========================================
+// JAVASCRIPT DESDE CERO
+// Capítulo 005 - Operadores
+// ==========================================
+
+
+// Declaramos las variables.
+const numero1 = 10;
+const numero2 = 5;
+
+
+// OPERADORES ARITMÉTICOS
+
+console.log(numero1 + numero2); // 15
+console.log(numero1 - numero2); // 5
+console.log(numero1 * numero2); // 50
+console.log(numero1 / numero2); // 2
+console.log(numero1 % numero2); // 0
+
+
+// OPERADORES DE COMPARACIÓN
+
+const edad = 20;
+
+console.log(edad > 18);   // true
+console.log(edad < 18);   // false
+console.log(edad >= 18);  // true
+console.log(edad === 20); // true
+console.log(edad !== 20); // false
+
+
+// OPERADORES LÓGICOS
+
+const tieneEdad = true;
+const tieneEntrada = true;
+
+console.log(tieneEdad && tieneEntrada); // true
+console.log(tieneEdad || tieneEntrada); // true
+console.log(!tieneEdad);                // false

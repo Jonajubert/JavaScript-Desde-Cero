f91e284c070d129c672ffbab046f3d22f67f6df6
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-08-26 21:03:52 -0300

    Add conditional evaluation for student grades

---
+++ 006-Condicionales/script.js
@@ -0,0 +1,24 @@
+// ==========================================
+// JAVASCRIPT DESDE CERO
+// Capítulo 006 - Condicionales
+// ==========================================
+
+
+// Declaramos la nota.
+const nota = 8;
+
+
+// Evaluamos diferentes posibilidades.
+if (nota >= 9) {
+
+    console.log("Excelente");
+
+} else if (nota >= 6) {
+
+    console.log("Aprobado");
+
+} else {
+
+    console.log("Desaprobado");
+
+}

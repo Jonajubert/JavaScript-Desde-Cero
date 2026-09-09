a0fcc55bf92dfb633b338785272fa966f24aa97d
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-09-09 20:28:38 -0300

    Add while loop example in script.js

---
+++ 008-Bucle-while/script.js
@@ -0,0 +1,22 @@
+// ==========================================
+// JAVASCRIPT DESDE CERO
+// Capítulo 008 - Bucle while
+// ==========================================
+
+
+// Declaramos e inicializamos el contador.
+let contador = 1;
+
+
+// Repetimos mientras la condición sea verdadera.
+while (contador <= 5) {
+
+    // Mostramos el valor actual.
+    console.log(`Número: ${contador}`);
+
+    // Incrementamos el contador.
+    contador++;
+}
+
+
+console.log("Fin del programa.");
